056be9f4d6b2a0ba5263cdd7e29a6b77c7847b64
Shashank-jais <shashankjaiswal266@gmail.com>
2025-03-15 20:58:33 +0530

    url updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express();
 app.use(cors({
   credentials:true,
-  origin:"https://ujala-shashank-jais-projects.vercel.app"
+  origin:"https://pressledger.vercel.app/"
 }));
 
 app.use(express.json())
